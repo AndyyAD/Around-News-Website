2d962cf6f5b89ffba0f12c918273b6e3c1fb809b
Andy <ashishdeb1231@gmail.com>
2023-03-15 19:32:27 +0530

    Added favicon

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
 				<title> Around - A News Website </title>
 				<meta name="description" content="Around - Find out what's going on around the world" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="/favicon.png" />
 			</Head>
 			<main className={styles.main}>
 				
