880b1bd870bf03248c0a2e7ef7fe5b248d454a7b
Andy <ashishdeb1231@gmail.com>
2023-03-16 15:54:34 +0530

    Added logo, navbar

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,24 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.png" />
 			</Head>
+
+			<nav className={styles.titleNav}>
+				<h1 className={styles.logo}> Around </h1>
+			</nav>
+
+			<nav className={styles.newsFliter}>
+				<ul className={styles.filterList}>
+					<li> <a href=""> Home </a> </li>
+					<hr/>
+					<li> <a href=""> India </a> </li>
+					<li> <a href=""> World </a> </li>
+					<li> <a href=""> Business </a> </li>
+					<li> <a href=""> Sports </a> </li>
+					<li> <a href=""> Entertainment </a> </li>
+					<li> <a href=""> Health </a> </li>
+				</ul>
+			</nav>
+
 			<main className={styles.main}>
 				
 			</main>
