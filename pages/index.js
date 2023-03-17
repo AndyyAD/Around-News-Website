331640438ae327a0843fdf06f51b07e7cf6720d2
Andy <ashishdeb1231@gmail.com>
2023-03-17 16:08:33 +0530

    Added utilities style

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,41 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
+import utils from '@/styles/utils.module.css'
 
 export default function Home() {
 	return (
 		<>
-			<Head>
-				<title> Around - A News Website </title>
-				<meta name="description" content="Around - Find out what's going on around the world" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.png" />
-			</Head>
+			<div className={utils.container}>
+				<Head>
+					<title> Around - A News Website </title>
+					<meta name="description" content="Around - Find out what's going on around the world" />
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<link rel="icon" href="/favicon.png" />
+				</Head>
 
-			<nav className={styles.titleNav}>
-				<h1 className={styles.logo}> Around </h1>
-			</nav>
+				<nav className={styles.titleNav}>
+					<h1 className={styles.logo}> Around </h1>
+				</nav>
 
-			<nav className={styles.newsFliter}>
-				<ul className={styles.filterList}>
-					<li> <a href=""> Home </a> </li>
-					<hr/>
-					<li> <a href=""> India </a> </li>
-					<li> <a href=""> World </a> </li>
-					<li> <a href=""> Business </a> </li>
-					<li> <a href=""> Sports </a> </li>
-					<li> <a href=""> Entertainment </a> </li>
-					<li> <a href=""> Health </a> </li>
-				</ul>
-			</nav>
+				<nav className={styles.newsFliter}>
+					<ul className={styles.filterList}>
+						<li> <Link href=""> Home </Link> </li>
+						<hr/>
+						<li> <Link href="/newspages/India"> India </Link> </li> 
+						<li> <Link href=""> World </Link> </li> 
+						<li> <Link href=""> Business </Link> </li> 
+						<li> <Link href=""> Sports </Link> </li> 
+						<li> <Link href=""> Entertainment </Link> </li> 
+						<li> <Link href=""> Health </Link> </li> 
+					</ul>
+				</nav>
 
-			<main className={styles.main}>
-				
-			</main>
+				<main className={styles.main}>
+					
+				</main>
+			</div>
 		</>
 	)
 }
---
+++ pages/newspages/India.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const India = () => {
+    return (
+        <div>I love my India</div>
+    )
+}
+
+export default India    
